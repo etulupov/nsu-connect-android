4e419576b446a953705dc28b67de9f59dd247ea9
Eugeny Tulupov <zhekka3@gmail.com>
2013-10-25 00:17:51 +0700

    Try to use google analytics

--- nsu-connect/src/main/java/ru/tulupov/nsuconnect/NsuConnectApplication.java
+++ nsu-connect/src/main/java/ru/tulupov/nsuconnect/NsuConnectApplication.java
@@ -2,6 +2,12 @@ package ru.tulupov.nsuconnect;
 
 import android.app.Application;
 
+import com.google.analytics.tracking.android.EasyTracker;
+import com.google.analytics.tracking.android.ExceptionReporter;
+import com.google.analytics.tracking.android.GAServiceManager;
+import com.google.analytics.tracking.android.GoogleAnalytics;
+import com.google.analytics.tracking.android.Tracker;
+
 import ru.tulupov.nsuconnect.database.HelperFactory;
 
 
@@ -9,6 +15,20 @@ public class NsuConnectApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Thread.UncaughtExceptionHandler handler = new ExceptionReporter(
+                EasyTracker.getInstance(this),
+                GAServiceManager.getInstance(),
+                Thread.getDefaultUncaughtExceptionHandler(),
+                this);
+
+        Thread.setDefaultUncaughtExceptionHandler(handler);
+
+
+
+
         HelperFactory.setHelper(getApplicationContext());
+
+
     }
 }
--- nsu-connect/src/main/java/ru/tulupov/nsuconnect/activity/SplashActivity.java
+++ nsu-connect/src/main/java/ru/tulupov/nsuconnect/activity/SplashActivity.java
@@ -7,6 +7,8 @@ import android.os.Message;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
+import com.google.analytics.tracking.android.EasyTracker;
+
 import ru.tulupov.nsuconnect.R;
 import ru.tulupov.nsuconnect.helper.IntentActionHelper;
 
@@ -25,11 +27,19 @@ public class SplashActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_splash);
 
-        Animation animation = AnimationUtils.loadAnimation(this,  R.anim.logo_in);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.logo_in);
         findViewById(R.id.logo).startAnimation(animation);
 
         handler.postDelayed(runnable, DELAY);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        EasyTracker.getInstance(this).activityStart(this);
     }
 }
