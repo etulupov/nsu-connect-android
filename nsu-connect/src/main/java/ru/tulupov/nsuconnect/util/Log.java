73ce0d5a04fb9a69d4bfef88662bac4769f9094d
Eugeny Tulupov <Eugeny.Tulupov@noveogroup.com>
2013-10-30 19:42:22 +0700

    Added logging of exceptions

--- nsu-connect/src/main/java/ru/tulupov/nsuconnect/util/Log.java
+++ nsu-connect/src/main/java/ru/tulupov/nsuconnect/util/Log.java
@@ -1,13 +1,38 @@
 package ru.tulupov.nsuconnect.util;
 
 
+import android.content.Context;
+
+import com.bugsense.trace.BugSenseHandler;
+import com.google.analytics.tracking.android.EasyTracker;
+import com.google.analytics.tracking.android.MapBuilder;
+
 public class Log {
-    public static void e(String tag, String msg) {
-        android.util.Log.e(tag, msg);
+    private static Context CONTEXT;
+
+    public static void init(Context context) {
+        CONTEXT = context.getApplicationContext();
     }
 
-    public static void e(String tag, String msg, Throwable tr) {
+
+    public static void e(String tag, String msg) {
+        android.util.Log.e(tag, msg);
+
+        EasyTracker.getInstance(CONTEXT).send(MapBuilder.createException(String.format("%s: %s", tag, msg), false).build());
+        BugSenseHandler.addCrashExtraData(tag, msg);
+    }
+
+    public static void e(String tag, String msg, Exception tr) {
         android.util.Log.e(tag, msg, tr);
+
+        EasyTracker.getInstance(CONTEXT).send(MapBuilder.createException(String.format("%s: %s: %s",
+                tag,
+                msg,
+                android.util.Log.getStackTraceString(tr)),
+                false).build());
+
+
+        BugSenseHandler.sendExceptionMessage(tag, msg, tr);
     }
 
     public static void v(String tag, String msg) {
