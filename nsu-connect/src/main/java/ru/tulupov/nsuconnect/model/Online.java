65597f232694fd894c3671b7b3d6b0e84a64cfa9
Eugeny Tulupov <zhekka3@gmail.com>
2013-11-18 01:38:38 +0700

    Change online model

--- nsu-connect/src/main/java/ru/tulupov/nsuconnect/model/Online.java
+++ nsu-connect/src/main/java/ru/tulupov/nsuconnect/model/Online.java
@@ -7,7 +7,29 @@ public class Online {
     @SerializedName("online")
     private int count;
 
+    @SerializedName("men")
+    private int menCount;
+
+    @SerializedName("women")
+    private int womenCount;
+
+    @SerializedName("unknown")
+    private int unknownCount;
+
+
     public int getCount() {
         return count;
+    }
+
+    public int getMenCount() {
+        return menCount;
+    }
+
+    public int getWomenCount() {
+        return womenCount;
+    }
+
+    public int getUnknownCount() {
+        return unknownCount;
     }
 }
